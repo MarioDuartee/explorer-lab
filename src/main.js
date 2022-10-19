5f31c13e4172b6f70f3c8846217317ef4446e385
Mario Duarte <marioduartee@hotmail.com>
2022-10-19 16:09:57 -0300

    final da aula1

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
 import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(".cc-bg  svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg  svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+function setCardType(type) {
+  const colors = {
+    visa: ["#2D57F2", "#436D99"],
+    mastercard: ["#C69347", "#DF6F29"],
+    amex: ["#29B3DF", "#68C647"],
+    elo: ["#FF0000", "#2D42FF"],
+    hipercard: ["#F22D2D", "#F22D2D"],
+  }
+  ccBgColor01.setAttribute("fill", colors[type][0]);
+  ccBgColor01.setAttribute("fill", colors[type][1]);
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
+
+}
+
+globalThis.setCardType = setCardType
+
+
